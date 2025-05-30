4223028419a31779f27463aa04d38a9740ee0ddc
hyeeun <silver807@pusan.ac.kr>
2025-05-30 15:40:40 +0900

    feat: Spring Boot 초기 설정

---
+++ backend/Tudy/src/main/java/com/example/tudy/TudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TudyApplication.class, args);
+    }
+
+}
