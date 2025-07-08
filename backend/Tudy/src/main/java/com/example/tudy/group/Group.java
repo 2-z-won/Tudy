4c005ef67b1f37690271a2a97a68cf24748996c9
Jiwon Lee <148948672+2-z-won@users.noreply.github.com>
2025-07-08 16:37:33 +0900

    Implement My Page backend features

---
+++ backend/Tudy/src/main/java/com/example/tudy/group/Group.java
@@ -0,0 +1,24 @@
+package com.example.tudy.group;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "is_private")
+    private boolean isPrivate;
+
+    private String password;
+}
